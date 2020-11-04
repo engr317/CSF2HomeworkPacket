4a11122f1f58bac06a67efcfe2d01b6dce017d1b
David See <engr12b10@hotmail.com>
2020-11-03 18:09:35 -0600

    added Student S1 with fields and properties.

--- ClassesLibrary/Login.cs
+++ ClassesLibrary/Login.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassesLibrary
 {
-    public class Class1
+    class Login
     {
     }
 }
---
+++ TesterProgram/TestHarness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.LastName = "Wayne";
+            s1.FirstName = "Bruce";
+            s1.Id = "BWayne";
+            s1.Gpa = 3.75f;
+
+            Console.WriteLine("{0} {1} with a school id of {2}, has graduated with a GPA of {3}. ", s1.FirstName, s1.LastName, s1.Id, s1.Gpa);
+        }
+    }
+}
